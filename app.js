bdac9b5f0d72a27831c2c665836d834bd31f67e5
abhigyanghosh30 <abhigyanghosh30@gmail.com>
2018-09-12 12:19:06 +0100

    File upload works

--- app.js
+++ app.js
@@ -79,29 +79,14 @@ app.get('/home',(req,res)=>{
 
 app.post('/upload',(req,res)=>{
 	var form = new formidable.IncomingForm();
-    form.parse(req, function(err, fields, files) {
-        // `file` is the name of the <input> field of type `file`
-        var old_path = files.file.path,
-            file_size = files.file.size,
-            file_ext = files.file.name.split('.').pop(),
-            index = old_path.lastIndexOf('/') + 1,
-            file_name = old_path.substr(index),
-            new_path = path.join(process.env.PWD, '/uploads/', file_name + '.' + file_ext);
-
-        fs.readFile(old_path, function(err, data) {
-            fs.writeFile(new_path, data, function(err) {
-                fs.unlink(old_path, function(err) {
-                    if (err) {
-                        res.status(500);
-                        res.send("File Upload Failed");
-                    } else {
-                        res.status(200);
-                        res.send("File Uploaded Successfully");
-                    }
-                });
-            });
-        });
+	form.parse(req);
+	form.on('fileBegin', function (name, file){
+        file.path = __dirname + '/uploads/' + file.name;
     });
+	form.on('file',(name,file)=>{
+		console.log('Uploaded'+file.name);
+	});
+    res.redirect('/home');
 });
 
 // PORT
