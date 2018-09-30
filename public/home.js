94c5282da92c344ada806357f0fe9b7e2dd4c372
abhigyanghosh30 <abhigyanghosh30@gmail.com>
2018-09-30 06:23:24 +0100

    added client side valdation for file size

--- public/home.js
+++ public/home.js
@@ -1,11 +1,20 @@
+var MB_LIMIT = 50 * 1024 * 1024;
+
 window.onload = function() {
     var $fileInput = this.document.getElementsByClassName(
             "custom-file-input"
         )[0],
+        DEFAULT_LABEL = "Choose file",
         $fileInputLabel = $fileInput.nextElementSibling;
 
     $fileInput.oninput = function() {
         $fileInputLabel.innerHTML =
-            this.value.replace(/.*[\/\\]/, "") || "Choose file";
+            this.value.replace(/.*[\/\\]/, "") || DEFAULT_LABEL;
+
+        if(this.files[0].size > MB_LIMIT){
+            alert("File upload of size above 50MB not allowed.");
+            this.value = "";
+            $fileInputLabel.innerHTML = DEFAULT_LABEL;
+        }
     };
 };
